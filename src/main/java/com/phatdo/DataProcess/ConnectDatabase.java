2b6d11f6bb5c5d6c3c81632c74c670fa7b9599a2
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2023-12-08 01:06:37 +0700

    search feature almost completed

--- src/main/java/com/phatdo/DataProcess/ConnectDatabase.java
+++ src/main/java/com/phatdo/DataProcess/ConnectDatabase.java
@@ -11,6 +11,15 @@ public class ConnectDatabase {
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "dotanphat";
 
+    private static int user_id;
+    public static void main(String[] args) {
+        try {
+            System.out.println(findApplication("Udemy"));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     public static boolean checkAuthenticate(String username, String password) throws SQLException{
         try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);) {
             // Establish a connection
@@ -23,10 +32,13 @@ public class ConnectDatabase {
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     // Process the ResultSet
                     while (resultSet.next()) {
+                        int user_id_tmp = resultSet.getInt("owner_id");
                         String username_tmp = resultSet.getString("owner_name");
                         String password_tmp = CryptographyTest.decrypt(resultSet.getString("password"));
-                        if (username_tmp.equals(username) && password_tmp.equals(password))
+                        if (username_tmp.equals(username) && password_tmp.equals(password)) {
+                            user_id = user_id_tmp;
                             return true;
+                        }
                     }
                 }
             } catch (Exception e) {
@@ -36,42 +48,33 @@ public class ConnectDatabase {
             return false;
         }
     }
-    public static void findApplication(String expected_application) throws SQLException {
+    public static String findApplication(String expected_application) throws SQLException {
+        String result = "";
         try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);) {
             // Establish a connection
 
             // Your SQL query
             String sqlQuery = "SELECT * FROM applications " +
-                    "WHERE application='"+expected_application+"';";
+                    "WHERE application='"+expected_application+"' AND owner_id=" +user_id + " ;";
 
             // Create a PreparedStatement
             try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
                 // Execute the query and obtain a ResultSet
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    // Process the ResultSet
                     while (resultSet.next()) {
-                        // Retrieve data from each row
                         int ownerId = resultSet.getInt("owner_id");
                         String application = resultSet.getString("application");
                         String username = resultSet.getString("username");
                         String password = resultSet.getString("password");
                         password = CryptographyTest.decrypt(password);
-                        // Add more columns as needed
-
-                        // Process the data as needed (e.g., print or use it)
-                        System.out.println("Owner ID: " + ownerId);
-                        System.out.println("Application: " + application);
-                        System.out.println("Username: " + username);
-                        System.out.println("Password: " + password);
-                        // Print or use additional columns
-
-                        System.out.println("----------");
+                        result = String.format("%s\nUsername: %s\nPassword: %s", application, username, password);
                     }
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
             }
         }
+        return result;
     }
     }
 
--- src/main/java/com/phatdo/PasswordManager/SearchFrame.java
+++ src/main/java/com/phatdo/PasswordManager/SearchFrame.java
@@ -1,19 +1,80 @@
 package com.phatdo.PasswordManager;
 
+import com.phatdo.DataProcess.ConnectDatabase;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 public class SearchFrame extends JFrame {
     private JLabel applicationLabel;
     private JTextField applicationTextField;
-    private JTextField information;
+    private JTextArea informationTextField;
+    private JButton findButton;
+
     public SearchFrame() {
         setTitle("Search for Application");
-        setSize(500,180);
+        setSize(400, 180);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         applicationLabel = new JLabel("Application");
         applicationTextField = new JTextField();
+        informationTextField = new JTextArea(3, 20);
+        findButton = new JButton("Find");
 
+        informationTextField.setLineWrap(true);
+        informationTextField.setWrapStyleWord(true);
+
+        // Set GridBagLayout
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        add(applicationLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        add(applicationTextField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.fill = GridBagConstraints.NONE;
+        add(new JLabel("Information"), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        add(informationTextField, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        add(findButton, gbc);
+
+        findButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performFind();
+            }
+        });
+    }
+
+    public void performFind() {
+        try {
+            String result = ConnectDatabase.findApplication(applicationTextField.getText());
+            informationTextField.setText(result);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
