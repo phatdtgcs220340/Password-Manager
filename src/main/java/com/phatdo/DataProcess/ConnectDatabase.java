848bc93decb2da9eedc7e8258fa1325540c413c2
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2023-12-09 03:02:13 +0700

    add adding application function

--- src/main/java/com/phatdo/DataProcess/ConnectDatabase.java
+++ src/main/java/com/phatdo/DataProcess/ConnectDatabase.java
@@ -2,6 +2,7 @@ package com.phatdo.DataProcess;
 import com.phatdo.Cryptography.CryptographyTest;
 
 import java.sql.*;
+import java.time.Instant;
 
 public class ConnectDatabase {
     // JDBC URL, username, and password of PostgreSQL server
@@ -13,60 +14,56 @@ public class ConnectDatabase {
     private static int user_id;
 
     public static boolean checkAuthenticate(String username, String password) throws SQLException {
-        try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);) {
-            // Establish a connection
-            // SQL query
-            String sqlQuery = "SELECT * FROM owners";
-
-            // Create a PreparedStatement
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
-                // Execute the query and obtain a ResultSet
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    // Process the ResultSet
-                    while (resultSet.next()) {
-                        int user_id_tmp = resultSet.getInt("owner_id");
-                        String username_tmp = resultSet.getString("owner_name");
-                        String password_tmp = CryptographyTest.decrypt(resultSet.getString("password"));
-                        if (username_tmp.equals(username) && password_tmp.equals(password)) {
-                            user_id = user_id_tmp;
-                            return true;
-                        }
-                    }
+        // SQL query
+        String sqlQuery = "SELECT * FROM owners";
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            // Process the ResultSet
+            while (resultSet.next()) {
+                int user_id_tmp = resultSet.getInt("owner_id");
+                String username_tmp = resultSet.getString("owner_name");
+                String password_tmp = CryptographyTest.decrypt(resultSet.getString("password"));
+                if (username_tmp.equals(username) && password_tmp.equals(password)) {
+                    user_id = user_id_tmp;
+                    return true;
                 }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
             }
-
-            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        return false;
     }
+
     public static String findApplication(String expected_application) throws SQLException {
         String result = "";
-        try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);) {
-            // Establish a connection
-
-            // Your SQL query
-            String sqlQuery = String.format("SELECT * FROM applications " +
-                    "WHERE application= '%s' AND owner_id= %d ;", expected_application, user_id);
-
-            // Create a PreparedStatement
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
-                // Execute the query and obtain a ResultSet
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    while (resultSet.next()) {
-                        int ownerId = resultSet.getInt("owner_id");
-                        String application = resultSet.getString("application");
-                        String username = resultSet.getString("username");
-                        String password = resultSet.getString("password");
-                        password = CryptographyTest.decrypt(password);
-                        result = String.format("%s\nUsername: %s\nPassword: %s", application, username, password);
-                    }
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+        String sqlQuery = String.format("SELECT * FROM applications " +
+                "WHERE application= '%s' AND owner_id= %d ;", expected_application, user_id);
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                int ownerId = resultSet.getInt("owner_id");
+                String application = resultSet.getString("application");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                password = CryptographyTest.decrypt(password);
+                result = String.format("%s\nUsername: %s\nPassword: %s", application, username, password);
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
         return result;
     }
+
+    public static void addApplication(String application, String username, String password) throws Exception {
+        String sqlQuery = String.format("INSERT INTO applications (owner_id, application, username, password, date_modified)" +
+                "VALUES (%d, '%s', '%s', '%s', '%s');", user_id, application, username, CryptographyTest.encrypt(password), Timestamp.from(Instant.now()));
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+            preparedStatement.executeUpdate();
+        }
     }
+}
 
--- src/main/java/com/phatdo/PasswordManager/PasswordManagerFrame.java
+++ src/main/java/com/phatdo/PasswordManager/PasswordManagerFrame.java
@@ -1,51 +1,162 @@
 package com.phatdo.PasswordManager;
 
+import com.phatdo.DataProcess.ConnectDatabase;
+import com.phatdo.Cryptography.PasswordGenerator;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class PasswordManagerFrame extends JFrame {
-    private JLabel applicationLabel;
-    private JTextField applicationTextField;
-    private JLabel userNameLabel;
-    private JTextField userNameTextField;
-    private JLabel passwordLabel;
-    private JTextField passwordTextField;
-
-    private JButton searchButton;
+    private JTextField addApplicationTextField;
+    private JTextField addUsernameTextField;
+    private JTextField addPasswordTextField;
     private JButton addButton;
-    private JButton deleteButton;
+
+    private JTextField searchApplicationTextField;
+    private JTextArea searchInformationTextField;
+    private JButton searchButton;
+    private JButton generatePasswordButton;
+
     public PasswordManagerFrame() {
         setTitle("Password Manager");
-        setSize(700, 180);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setVisible(true);
 
-        applicationLabel = new JLabel("Application");
-        applicationTextField = new JTextField();
-        userNameLabel = new JLabel("Username");
-        userNameTextField = new JTextField();
-        passwordLabel = new JLabel("Password");
-        passwordTextField = new JTextField();
-        searchButton = new JButton("Search");
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        // Add Tab
+        JPanel addPanel = new JPanel();
+        addPanel.setLayout(new GridLayout(5, 1, 10, 20));
+
+        addApplicationTextField = new JTextField();
+        addUsernameTextField = new JTextField();
+        addPasswordTextField = new JTextField();
         addButton = new JButton("Add");
-        deleteButton = new JButton("Delete");
-        setLayout(new GridLayout(4, 3));
+        addButton.setPreferredSize(new Dimension((int) (getWidth() * 0.8), addButton.getHeight()));
+        generatePasswordButton = new JButton("Generate Password");
+        generatePasswordButton.setPreferredSize(new Dimension((int) (getWidth() * 0.8), generatePasswordButton.getHeight()));
 
-        add(new JLabel());
-        add(applicationLabel);
-        add(applicationTextField);
+        addPanel.add(createPanel(new JLabel("Application: "), addApplicationTextField));
+        addPanel.add(createPanel(new JLabel("Username:  "), addUsernameTextField));
+        addPanel.add(createPanel(new JLabel("Password:  "), addPasswordTextField));
+        addPanel.add(generatePasswordButton);
+        addPanel.add(addButton);
 
-        add(new JLabel());
-        add(userNameLabel);
-        add(userNameTextField);
+        generatePasswordButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performGeneratePassword();
+            }
+        });
 
-        add(new JLabel());
-        add(passwordLabel);
-        add(passwordTextField);
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performAdd();
+            }
+        });
+        // Search Tab
+        JPanel searchPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
-        add(searchButton);
-        add(addButton);
-        add(deleteButton);
+        searchApplicationTextField = new JTextField();
+        searchInformationTextField = new JTextArea(3, 20);
+        searchButton = new JButton("Search");
+
+        searchInformationTextField.setLineWrap(true);
+        searchInformationTextField.setWrapStyleWord(true);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        searchPanel.add(new JLabel("Application"), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        searchPanel.add(searchApplicationTextField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.fill = GridBagConstraints.NONE;
+        searchPanel.add(new JLabel("Information"), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        searchPanel.add(searchInformationTextField, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        searchPanel.add(searchButton, gbc);
+
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performSearch();
+            }
+        });
+        // Add tabs to the tabbedPane
+        tabbedPane.addTab("Add Application", addPanel);
+        tabbedPane.addTab("Search Application", searchPanel);
+
+        // Add tabbedPane to the frame
+        add(tabbedPane);
+
+        // Set up the frame
+        setSize(400, 300);
+        setVisible(true);
+    }
+
+    private JPanel createPanel(JLabel label, JComponent component) {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(label, BorderLayout.WEST);
+        panel.add(component, BorderLayout.CENTER);
+        return panel;
+    }
+
+    public void performSearch() {
+        try {
+            String result = ConnectDatabase.findApplication(searchApplicationTextField.getText());
+            if (Objects.equals(result, "")) {
+                ErrorMessage.showErrorDialog("Application doesn't exist","Invalid input");
+            }
+            searchInformationTextField.setText(result);
+        } catch (SQLException e) {
+            ErrorMessage.showErrorDialog("Coudln't connect to database :((", "Connection error");
+        }
+    }
+
+    public void performGeneratePassword() {
+        addPasswordTextField.setText(PasswordGenerator.generatePassword(16));
+    }
+    public void performAdd() {
+        try {
+            String application = addApplicationTextField.getText();
+            String username = addUsernameTextField.getText();
+            String password = addPasswordTextField.getText();
+            ConnectDatabase.addApplication(application, username, password);
+        }
+//        catch ( e) {
+//            System.out.println("Unique constraint violated");
+//        }
+        catch (SQLException e) {
+            ErrorMessage.showErrorDialog("Coudln't connect to database :((", "Connection error");
+        }
+        catch (Exception e) {
+            ErrorMessage.showErrorDialog("Couldn't get Key", "Key Error");
+        }
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new PasswordManagerFrame());
     }
 }
