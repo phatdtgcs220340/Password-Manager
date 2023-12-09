2d8ccced8256455acf469941ea540c2f4d7537ed
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2023-12-09 15:20:34 +0700

    modify it for general using of dialog message

--- src/main/java/com/phatdo/PasswordManager/DialogMessage.java
+++ src/main/java/com/phatdo/PasswordManager/DialogMessage.java
@@ -2,9 +2,12 @@ package com.phatdo.PasswordManager;
 
 import javax.swing.*;
 
-public class ErrorMessage {
+public class DialogMessage {
     public static void showErrorDialog(String message, String title) {
         JOptionPane.showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
+    }
+    public static void showNotificationDialog(String message, String title) {
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
     }
 
 }
