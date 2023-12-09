b9ea623373accdbf21540766f02b4e5c1595870e
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2023-12-09 15:21:21 +0700

    fix empty text bug

--- src/main/java/com/phatdo/StringFormatter/StringFormat.java
+++ src/main/java/com/phatdo/StringFormatter/StringFormat.java
@@ -1,10 +1,15 @@
 package com.phatdo.StringFormatter;
 
+import java.util.Objects;
+
 public class StringFormat {
     public static void main(String[] args) {
-        System.out.println(toCapitalize("concac a"));
+        System.out.println(toCapitalize(""));
     }
     public static String toCapitalize(String text) {
+        if (Objects.equals(text, "")) {
+            return "";
+        }
         return text.substring(0, 1).toUpperCase() + text.substring(1);
     }
 
