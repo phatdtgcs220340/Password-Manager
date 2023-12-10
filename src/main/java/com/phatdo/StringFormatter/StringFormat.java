24ddb8de3c0ec7f8a3ebf8e9c684eed6d51aa5d7
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2023-12-10 15:31:37 +0700

    add new method trim text and add encode text

--- src/main/java/com/phatdo/StringFormatter/StringFormat.java
+++ src/main/java/com/phatdo/StringFormatter/StringFormat.java
@@ -3,14 +3,24 @@ package com.phatdo.StringFormatter;
 import java.util.Objects;
 
 public class StringFormat {
-    public static void main(String[] args) {
-        System.out.println(toCapitalize(""));
-    }
     public static String toCapitalize(String text) {
+        text = removeLeadingTrailingSpaces(text);
         if (Objects.equals(text, "")) {
             return "";
         }
         return text.substring(0, 1).toUpperCase() + text.substring(1);
     }
 
+    public static String removeLeadingTrailingSpaces(String input) {
+        // Use the trim() method to remove leading and trailing spaces
+        return input.trim();
+    }
+
+    public static String encodeText(char character, int times) {
+        StringBuilder result = new StringBuilder();
+
+        result.append(String.valueOf(character).repeat(Math.max(0, times)));
+
+        return result.toString();
+    }
 }
