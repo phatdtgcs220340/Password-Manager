791e585346fa7249444bbc38e08595c92bb99154
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2023-12-23 09:22:16 +0700

    beautify code

--- src/main/java/com/phatdo/Main.java
+++ src/main/java/com/phatdo/Main.java
@@ -1,11 +1,11 @@
 package com.phatdo;
 
 import com.phatdo.Authentication.Authentication;
+import com.phatdo.FrameFormatter.FrameFormat;
 import com.phatdo.Login.LoginFrame;
 import com.phatdo.PasswordManager.PasswordManagerFrame;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Main {
 
@@ -15,25 +15,12 @@ public class Main {
     }
 
     public void go() {
-        UIManager.put("Label.foreground", Color.decode("#7C93C3"));
-        UIManager.put("TextField.foreground", Color.decode("#164863"));
-        UIManager.put("ComboBox.foreground", Color.decode("#164863"));
-        UIManager.put("ComboBox.background", Color.decode("#9EB8D9"));
-        UIManager.put("TextArea.foreground", Color.decode("#164863"));
-        UIManager.put("Button.foreground", Color.decode("#164863"));
-        UIManager.put("Button.background", Color.decode("#9EB8D9"));
+
+        FrameFormat.setUI();
+
         JFrame loginFrame = new LoginFrame();
         loginFrame.setVisible(true);
-        boolean isAuthenticated;
-        do {
-            isAuthenticated = Authentication.getAuthenticate();
-            try {
-                // Sleep for 0.2 seconds (200 milliseconds)
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } while (!isAuthenticated);
+        Authentication.authenticationLoop();
         loginFrame.setVisible(false);
         JFrame pasManFrame = new PasswordManagerFrame();
         pasManFrame.setVisible(true);
