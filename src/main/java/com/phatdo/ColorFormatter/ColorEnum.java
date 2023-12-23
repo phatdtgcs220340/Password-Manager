40680768d83220a42cdc6c563e05dac885f93f04
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2023-12-23 09:18:23 +0700

    add color enum for better using

---
+++ src/main/java/com/phatdo/ColorFormatter/ColorEnum.java
@@ -0,0 +1,29 @@
+package com.phatdo.ColorFormatter;
+
+import java.awt.*;
+
+public enum ColorEnum {
+    TEXTFIELD("#0B0B0D", "#FFFFFF"),
+    LABEL("#474A56", "#D3D5FD"),
+    BACKGROUND("#D3D5FD", "#D3D5FD"),
+    TITLEBAR("#0B0B0D", "#FFFFFF"),
+    OPTION_PANE("#474A56", "#D3D5FD"),
+    COMBO_BOX("#474A56", "#FFFFFF"),
+    BUTTON("#474A56", "#929AAB");
+
+    private Color fgColor;
+    private Color bgColor;
+
+    ColorEnum(String fgColorCode, String bgColorCode) {
+        fgColor = Color.decode(fgColorCode);
+        bgColor = Color.decode(bgColorCode);
+    }
+
+    public Color getFgColor() {
+        return fgColor;
+    }
+
+    public Color getBgColor() {
+        return bgColor;
+    }
+}
